24a015a84339be5e94f741603c090234d3666877
Artur <dwojaka207@gmail.com>
2021-04-29 23:01:12 +0200

    Modifying for base_url

--- main.py
+++ main.py
@@ -4,35 +4,37 @@ from urllib3 import PoolManager
 
 class BaseRequest(ABC):
 
+    ALLOWED_METHODS = ["GET"]
+    BASE_URL = None
+
     @abstractmethod
     def ENDPOINT(self):
         """
         Speficic endpoint mixed up with base url from router object
-        Eg. '/anything'
+        Eg. "/anything"
         """
-    ALLOWED_METHODS = ['GET']
 
-    def __init__(self, router=None):
+    def __init__(self, base_url=""):
         self.http = PoolManager()
-        # setattr(router, 'aaa', self)
-        # router.__dict__['aaa'] = self
+        self.set_base_url(base_url)
+
+    def set_base_url(self, base_url):
+        self.BASE_URL = base_url
+
+    def url(self):
+        return f"{self.BASE_URL}{self.ENDPOINT}"
 
     def _request(self, method):
         if method in self.ALLOWED_METHODS:
             return self.http.request(method, self.ENDPOINT)
         else:
-            raise Exception('Method not allowed')
+            raise Exception("Method not allowed")
 
     def get(self):
-        return self._request('GET')
+        return self._request("GET")
 
     def post(self):
-        return self._request('POST')
-
-
-class HttpBin(BaseRequest):
-    ENDPOINT = '/anything'
-    ALLOWED_METHODS = ['GET', 'POST']
+        return self._request("POST")
 
 
 class BaseRouter(ABC):
@@ -41,46 +43,33 @@ class BaseRouter(ABC):
     def BASE_URL(self):
         """
         Base url mixed with specific endpoints
-        Eg. 'https://httpbin.org/'
+        Eg. "https://httpbin.org/"
         """
 
-    def __init__(self, **kwargs):
-        self.keys = kwargs.keys()
-        for name, value in kwargs.items():
-            setattr(self, name, value)
 
-    def __getattr__(self, attr):
-        # _module = self._resolve()
-        # value = getattr(_module, attr)
-        # setattr(self, attr, value)
-        return self.__getattr__(attr)
-
-    def __dir__(self):
-        return self.keys
-        # import pdb;pdb.set_trace()
-        # return self.df.keys()
-
-    # def __dir__(self):
-    #     import pdb;pdb.set_trace()
-        # return self.__dir__()
-
-    # http_bin = HttpBin()
+class Anything(BaseRequest):
+    ENDPOINT = "/anything"
+    ALLOWED_METHODS = ["GET", "POST"]
 
 
 class HttpRouter(BaseRouter):
-    BASE_URL = 'https://httpbin.org/'
+    BASE_URL = "https://httpbin.org/"
+
+    anything = Anything()
+
+    def __init__(self):
+        all_props = self.__dir__()
+        for prop_name in all_props:
+            prop = getattr(self, prop_name)
+            if isinstance(prop, BaseRequest):
+                print('tak')
 
 
 def tmp_display(byte_data):
     import json
-    return json.loads(byte_data.decode('utf-8'))
+    return json.loads(byte_data.decode("utf-8"))
 
 
-# http_bin = HttpBin()
-# print(tmp_display(http_bin.get().data))
-# print(tmp_display(http_bin.post().data))
-
-http_bin = HttpBin()
-qwe = HttpRouter(asd=http_bin)
-print(qwe)
-# print(qwe.http_bin.get().data)
+# aa = Anything()
+# print(aa.url())
+HttpRouter()
