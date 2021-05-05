1d8d55f1d62fb353fa00e0e880c03d2136c109b9
adwojak <dwojaka207@gmail.com>
2021-05-05 21:35:17 +0200

    Modified jwt request and added finalize method

--- base_request.py
+++ base_request.py
@@ -23,15 +23,18 @@ class HttpMethodHandler(ABC):
 
     def get(self, **kwargs):
         response = self._request('GET', **kwargs)
-        return self.handle_get(response)
+        response = self.handle_get(response)
+        return self.finalize(response)
 
     def options(self, **kwargs):
         response = self._request('OPTIONS', **kwargs)
-        return self.handle_options(response)
+        response = self.handle_options(response)
+        return self.finalize(response)
 
     def head(self, **kwargs):
         response = self._request('HEAD', **kwargs)
-        return self.handle_head(response)
+        response = self.handle_head(response)
+        return self.finalize(response)
 
     def post(self, data=None, json=None, **kwargs):
         kwargs.update({
@@ -39,25 +42,32 @@ class HttpMethodHandler(ABC):
             'json': json,
         })
         response = self._request("POST", **kwargs)
-        return self.handle_post(response)
+        response = self.handle_post(response)
+        return self.finalize(response)
 
     def put(self, data=None, **kwargs):
         kwargs.update({
             'data': data,
         })
         response = self._request("PUT", **kwargs)
-        return self.handle_put(response)
+        response = self.handle_put(response)
+        return self.finalize(response)
 
     def patch(self, data=None, **kwargs):
         kwargs.update({
             'data': data,
         })
         response = self._request("PATCH", **kwargs)
-        return self.handle_patch(response)
+        response = self.handle_patch(response)
+        return self.finalize(response)
 
     def delete(self, **kwargs):
         response = self._request('DELETE', **kwargs)
-        return self.handle_delete(response)
+        response = self.handle_delete(response)
+        return self.finalize(response)
+
+    def finalize(self, response):
+        return response
 
     def handle_get(self, response):
         return response
@@ -81,10 +91,16 @@ class HttpMethodHandler(ABC):
         return response
 
 
-class BaseRequest(HttpMethodHandler):
+class BaseRequest(HttpMethodHandler, ABC):
 
-    ENDPOINT = '/'
     ALLOWED_METHODS = ['GET']
+
+    @abstractmethod
+    def ENDPOINT(self):
+        """
+        Specific endpoint mixed with base url
+        Eg. "/anything"
+        """
 
     def __init__(self):
         # Session instance for requests sending
@@ -108,6 +124,43 @@ class BaseRequest(HttpMethodHandler):
         return self.session.request(method, self.url, **kwargs)
 
 
+class AuthBaseRequest(BaseRequest):
+
+    def __init__(self):
+        super().__init__()
+
+        self.access_token = None
+        self.refresh_token = None
+        self.expires_in = None
+
+    @abstractmethod
+    def set_access_token(self, json_data):
+        """
+        Returns access token from response object
+        """
+
+    @abstractmethod
+    def set_refresh_token(self, json_data):
+        """
+        Returns refresh token from response object
+        """
+
+    @abstractmethod
+    def set_expiration_time(self, json_data):
+        """
+        Returns expiration time from response object
+        """
+
+    def finalize(self, response):
+        self.set_tokens(response.json())
+        return response
+
+    def set_tokens(self, json_data):
+        self.access_token = self.set_access_token(json_data)
+        self.refresh_token = self.set_refresh_token(json_data)
+        self.expires_in = self.set_expiration_time(json_data)
+
+
 class ExampleRequest(BaseRequest):
     ENDPOINT = '/example'
     ALLOWED_METHODS = ['GET', 'POST']
@@ -118,6 +171,15 @@ class ArgumentsRequest(BaseRequest):
     ALLOWED_METHODS = ['POST', 'PUT']
 
 
-class JwtRequest(BaseRequest):
+class JwtRequest(AuthBaseRequest):
     ENDPOINT = '/jwt'
     ALLOWED_METHODS = ['GET', 'POST']
+
+    def set_access_token(self, json_data):
+        return json_data['access_token']
+
+    def set_refresh_token(self, json_data):
+        return json_data['refresh_token']
+
+    def set_expiration_time(self, json_data):
+        return json_data['expires_in']
